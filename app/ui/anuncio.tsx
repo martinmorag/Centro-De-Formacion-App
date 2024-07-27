a9e97d6bac5e6b51f521010833a58b86c78e29a9
Martin <moragamartin2@gmail.com>
2024-07-27 13:10:48 -0300

    align of noticias

--- app/ui/anuncio.tsx
+++ app/ui/anuncio.tsx
@@ -9,12 +9,12 @@ export const Anuncios: React.FC<HomePageProps> = ({ anuncios }) => {
     else return (
         <>
         {anuncios.map((anuncio) => (
-            <section key={anuncio._id} className="flex flex-col justify-between w-[90vw] h-[95vh] bg-back2 p-2 m-2 mx-6 rounded-md">
-                <div>
-                    <h1 className="text-center pt-3 pr-3 pl-3 text-2xl break-words font-bold">{anuncio.nombre}</h1>
+            <section key={anuncio._id} className="flex justify-between w-[90vw] h-[95vh] bg-back2 p-2 m-2 mx-6 rounded-md">
+                <div className="flex flex-col justify-center items-center w-[25vw] h-full px-4">
+                    <h1 className="text-center pb-5 pt-3 pr-3 pl-3 text-2xl break-words font-bold">{anuncio.nombre}</h1>
                     <p className="text-center pl-5 pr-5 pt-2 break-words font-bold">{anuncio.descripcion}</p>
                 </div>
-                <div className="flex justify-center items-end h-full overflow-hidden">
+                <div className="flex justify-center items-center items-end w-[70vw] px-4 w-full overflow-hidden">
                     <img 
                     src={anuncio.imagen} 
                     alt={anuncio.nombre} 
